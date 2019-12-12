c48b4c0e245d3d9e9c53529767c482b9dc611e51
TharunKumarReddyM <thanukumar007@gmail.com>
2019-12-12 13:52:51 +0530

    Added the CardView Item to Recycle Added some Colors

---
+++ app/src/main/java/com/sample/employeedetails/ui/MainActivityDashboard/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sample.employeedetails.ui.MainActivityDashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sample.employeedetails.R
+import com.sample.employeedetails.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun setLayout(): Int {
+        return R.layout.list_cardview_item
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+
+}

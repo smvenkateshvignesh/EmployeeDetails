b107b7474a3c39f07a421bfccbe5c22a3a9e742f
vvenkatesh <vvicky5270@gmail.com>
2019-12-12 10:11:08 +0530

    Testing two

--- app/src/main/java/com/sample/employeedetails/application/MyApplication.kt
+++ app/src/main/java/com/sample/employeedetails/application/MyApplication.kt
@@ -7,7 +7,7 @@ class MyApplication: Application() {
     init {
         myApplication = this
     }
-    //Vignesh
+    //Vignesh new
     companion object {
         private lateinit var myApplication: Application
         fun getApplicationContext(): Context {
